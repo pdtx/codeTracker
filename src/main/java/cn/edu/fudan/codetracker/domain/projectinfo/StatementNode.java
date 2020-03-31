334d9b147305cee084de8d92d5e0005432bff7f2
chenyuan <ranxiaoliang1996@126.com>
2020-03-31 18:04:12 +0800

    reconstruct methodNode

---
+++ src/main/java/cn/edu/fudan/codetracker/domain/projectinfo/StatementNode.java
@@ -0,0 +1,56 @@
+package cn.edu.fudan.codetracker.domain.projectinfo;
+
+import cn.edu.fudan.codetracker.domain.ProjectInfoLevel;
+import cn.edu.fudan.codetracker.domain.StatementType;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+public class StatementNode extends BaseNode{
+
+    private String body;
+    private int begin;
+    private int end;
+    /**
+     * 用于存储meta methodUuid?
+     */
+    private String methodUuid;
+    private int level;
+    private StatementType type;
+    private int sequence;
+
+    public StatementNode (String body, int begin, int end) {
+        super.setProjectInfoLevel(ProjectInfoLevel.STATEMENT);
+        this.body = body;
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.getUuid().hashCode();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if(o == null){
+            return false;
+        }
+
+        if(o instanceof StatementNode){
+            StatementNode statementNode = (StatementNode) o;
+            return this.getUuid().equals(statementNode.getUuid());
+        }
+        return false;
+    }
+
+}
