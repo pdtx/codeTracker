6d51c089986c9c7f8766d31a95a20254ecbdbc46
chenyuan <ranxiaoliang1996@126.com>
2020-05-28 14:29:24 +0800

    rename测试

--- src/main/java/cn/edu/fudan/codetracker/jgit/RenameAfter.java
+++ src/main/java/cn/edu/fudan/codetracker/jgit/RenameAfter.java
@@ -1,6 +1,6 @@
 package cn.edu.fudan.codetracker.jgit;
 
-public class Rename {
+public class RenameAfter {
     public static void main(String[] args) {
         int a = 2;
         int b = 3;
@@ -8,6 +8,6 @@ public class Rename {
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
-        System.out.println("rename");
+        System.out.println("rename after");
     }
 }
