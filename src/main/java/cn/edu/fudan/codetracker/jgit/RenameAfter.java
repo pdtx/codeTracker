3c1d31d271c57e040c6dd56204a8b9233977a918
chenyuan <ranxiaoliang1996@126.com>
2020-05-28 14:22:16 +0800

    rename再次测试

--- src/main/java/cn/edu/fudan/codetracker/jgit/RenameAfter.java
+++ src/main/java/cn/edu/fudan/codetracker/jgit/RenameAfter.java
@@ -2,6 +2,8 @@ package cn.edu.fudan.codetracker.jgit;
 
 public class RenameAfter {
     public static void main(String[] args) {
+        int a = 2;
+        System.out.println(a);
         System.out.println("rename after");
     }
 }
