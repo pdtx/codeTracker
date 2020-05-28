e43a8c19634265f5ff057183c54e8f13e132fd05
chenyuan <ranxiaoliang1996@126.com>
2020-05-28 14:28:21 +0800

    相似度超过65%rename测试

--- src/main/java/cn/edu/fudan/codetracker/jgit/Rename.java
+++ src/main/java/cn/edu/fudan/codetracker/jgit/Rename.java
@@ -3,7 +3,11 @@ package cn.edu.fudan.codetracker.jgit;
 public class Rename {
     public static void main(String[] args) {
         int a = 2;
+        int b = 3;
+        int c = a*b;
         System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
         System.out.println("rename");
     }
 }
