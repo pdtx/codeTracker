b51c18d770b37f4e2112bf119145fdcf35d94d6d
chenyuan <ranxiaoliang1996@126.com>
2020-06-06 21:27:12 +0800

    初步实现merge conflict情况处理 未debug

--- src/main/java/cn/edu/fudan/codetracker/service/impl/ScanServiceImpl.java
+++ src/main/java/cn/edu/fudan/codetracker/service/impl/ScanServiceImpl.java
@@ -12,6 +12,7 @@ import cn.edu.fudan.codetracker.core.tree.JavaTree;
 import cn.edu.fudan.codetracker.core.tree.Language;
 import cn.edu.fudan.codetracker.core.tree.RepoInfoTree;
 import cn.edu.fudan.codetracker.dao.*;
+import cn.edu.fudan.codetracker.domain.diff.DiffInfo;
 import cn.edu.fudan.codetracker.domain.projectinfo.*;
 import cn.edu.fudan.codetracker.jgit.JGitHelper;
 import cn.edu.fudan.codetracker.service.ScanService;
@@ -165,7 +166,7 @@ public class ScanServiceImpl implements ScanService {
 
         //merge情况直接跳过
         if (fileMap.keySet().size() > 1) {
-            dealWithMerge(jGitHelper,commitId,repoPath,outputPath);
+            dealWithMerge(jGitHelper,commitId,repoPath,outputPath,repoUuid,branch);
             return;
         }
 
@@ -214,7 +215,7 @@ public class ScanServiceImpl implements ScanService {
 
     }
 
-    private void dealWithMerge(JGitHelper jGitHelper, String commit, String repoPath, String outputPath) {
+    private void dealWithMerge(JGitHelper jGitHelper, String commit, String repoPath, String outputPath, String repoUuid, String branch) {
         Map<String, List<DiffEntry>> conflictInfo = jGitHelper.getConflictDiffEntryList(commit);
         if (conflictInfo == null || conflictInfo.size() == 0) {
             return;
@@ -238,9 +239,118 @@ public class ScanServiceImpl implements ScanService {
         Map<String,Map<String,String>> logicalChangedFileMap = extractDiffFilePathFromClDiff(repoPath,commit,outputPath);
         //构造三棵树
         List<String> fileList = new ArrayList<>();
+        for (DiffEntry diffEntry : list) {
+            if ("MODIFY".equals(diffEntry.getChangeType())) {
+                fileList.add(diffEntry.getNewPath());
+            }
+        }
+        Map<String,List<String>> map = new HashMap<>();
+        map.put("CHANGE",fileList);
+        CommonInfo preCommonInfo = constructCommonInfo(repoUuid,branch,parent1,null,jGitHelper);
+        CommonInfo curCommonInfo = constructCommonInfo(repoUuid,branch,commit,parent1,jGitHelper);
+        CommonInfo compareCommonInfo = constructCommonInfo(repoUuid,branch,parent2,null,jGitHelper);
+        jGitHelper.checkout(parent1);
+        RepoInfoTree preTree = new RepoInfoTree(fileList,preCommonInfo,repoUuid);
+        RepoInfoTree curTree = new RepoInfoTree(fileList,curCommonInfo,repoUuid);
+        RepoInfoTree compareTree = new RepoInfoTree(fileList,compareCommonInfo,repoUuid);
 
+        JavaTree preJavaTree = (JavaTree) preTree.getRepoTree().get(Language.JAVA);
+        JavaTree curJavaTree = (JavaTree) curTree.getRepoTree().get(Language.JAVA);
+        JavaTree compareJavaTree = (JavaTree) compareTree.getRepoTree().get(Language.JAVA);
+
+        TrackerCore.mapping(preJavaTree,curJavaTree,preCommonInfo,repoUuid,branch,map,logicalChangedFileMap,outputPath,parent1);
+
+        //与第三棵树对比ADD情况
+        compareWithParent2(curJavaTree,compareJavaTree);
+
+        //入库
+        extractAndSaveInfo(preJavaTree,curJavaTree,curCommonInfo);
 
     }
+
+
+    private void compareWithParent2(JavaTree curTree, JavaTree compareTree) {
+        for (ClassNode classNode : curTree.getClassInfos()) {
+            if (BaseNode.ChangeStatus.ADD.equals(classNode.getChangeStatus())) {
+                if (findSameNode(classNode,compareTree) != null) {
+                    classNode.setChangeStatus(BaseNode.ChangeStatus.UNCHANGED);
+                }
+            }
+        }
+
+        for (MethodNode methodNode : curTree.getMethodInfos()) {
+            if (BaseNode.ChangeStatus.ADD.equals(methodNode.getChangeStatus())) {
+                if (findSameNode(methodNode,compareTree) != null) {
+                    methodNode.setChangeStatus(BaseNode.ChangeStatus.UNCHANGED);
+                }
+            }
+        }
+
+        for (FieldNode fieldNode : curTree.getFieldInfos()) {
+            if (BaseNode.ChangeStatus.ADD.equals(fieldNode.getChangeStatus())) {
+                if (findSameNode(fieldNode,compareTree) != null) {
+                    fieldNode.setChangeStatus(BaseNode.ChangeStatus.UNCHANGED);
+                }
+            }
+        }
+
+        for (StatementNode statementNode : curTree.getStatementInfos()) {
+            if (BaseNode.ChangeStatus.ADD.equals(statementNode.getChangeStatus())) {
+                if (findSameNode(statementNode,compareTree) != null) {
+                    statementNode.setChangeStatus(BaseNode.ChangeStatus.UNCHANGED);
+                }
+            }
+        }
+
+    }
+
+
+    private BaseNode findSameNode(BaseNode baseNode, JavaTree compareTree) {
+        if(baseNode instanceof ClassNode) {
+            ClassNode classNode = (ClassNode)baseNode;
+            for (ClassNode node : compareTree.getClassInfos()) {
+                if (classNode.equals(node)) {
+                    return node;
+                }
+            }
+            return null;
+        }
+        if(baseNode instanceof MethodNode) {
+            MethodNode methodNode = (MethodNode)baseNode;
+            for (MethodNode node : compareTree.getMethodInfos()) {
+                if (methodNode.equals(node)) {
+                    return node;
+                }
+            }
+            return null;
+        }
+        if(baseNode instanceof FieldNode) {
+            FieldNode fieldNode = (FieldNode)baseNode;
+            for (FieldNode node : compareTree.getFieldInfos()) {
+                if (fieldNode.equals(node)) {
+                    return node;
+                }
+            }
+            return null;
+        }
+        if(baseNode instanceof StatementNode) {
+            StatementNode statementNode = (StatementNode)baseNode;
+            BaseNode root = findSameNode(statementNode.getParent(),compareTree);
+            if (root == null) {
+                return null;
+            } else {
+                for (BaseNode node : root.getChildren()) {
+                    StatementNode statement = (StatementNode)node;
+                    if (statement.equals(statementNode)) {
+                        return statement;
+                    }
+                }
+                return null;
+            }
+        }
+        return null;
+    }
+
 
     private CommonInfo constructCommonInfo(String repoUuid, String branch, String commit, String parentCommit, JGitHelper jGitHelper) {
         if (parentCommit == null || parentCommit.length() == 0) {
